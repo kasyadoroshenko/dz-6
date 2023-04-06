5191dd86f1749d2476054c986aa17820f24d90d6
kateryna.doroshenko <kasyadoroshenko@gmail.com>
2023-04-06 16:18:45 +0300

    homework 6 commit

--- src/Main.java
+++ src/Main.java
@@ -3,16 +3,16 @@ public class Main {
 
         int a = 10;
         int b = 15;
-        int с = 20;
+        int c = 20;
         int d = 25;
 
         int sum1 = a + b;
-        int sum2 = с + d;
+        int sum2 = c + d;
 
         System.out.println(sum1);
         System.out.println(sum2);
 
-        System.out.println(sum1 > sum2 ? "True" : "False");
+        System.out.println(sum1>sum2 ? "True": "False");
 
         sum1 += 1;
         sum2 -= 2;
@@ -20,9 +20,9 @@ public class Main {
         System.out.println(sum1);
         System.out.println(sum2);
 
-        System.out.println(sum1 > sum2 ? "True" : "False");
+        System.out.println(sum1>sum2 ? "True": "False");
 
-        System.out.println(sum1 % 2 < 1 || sum2 % 2 < 1 ? "True" : "False");
+        System.out.println(sum1%2 < 1  || sum2%2 < 1 ? "True": "False");
 
         System.out.println(sum1 % 2);
         System.out.println(sum2 % 2);
